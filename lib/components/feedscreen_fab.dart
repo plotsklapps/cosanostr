fbf242823cf2c3924b0ada037cb7882dadcc03c3
plotsklapps <plotsklapps@gmail.com>
2023-07-19 19:31:01 +0200

    - Wait, there is an isExtended parameter on FAB???

--- lib/components/feedscreen_fab.dart
+++ lib/components/feedscreen_fab.dart
@@ -25,165 +25,87 @@ class FeedScreenFABState extends ConsumerState<FeedScreenFAB> {
 
   @override
   Widget build(BuildContext context) {
-    if (!ref.watch(isUserScrollingProvider)) {
-      return FloatingActionButton.extended(
-        tooltip: 'Create a new Nost',
-        label: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('NEW'),
-            SizedBox(width: 8.0),
-            Icon(FontAwesomeIcons.featherPointed),
-          ],
-        ),
-        onPressed: () async {
-          noteController.clear();
-          await showModalBottomSheet<void>(
-            context: context,
-            isScrollControlled: true,
-            builder: (BuildContext context) {
-              return Padding(
-                padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      const Icon(FontAwesomeIcons.featherPointed),
-                      const SizedBox(height: 8.0),
-                      const Text(
-                        'Create a new Nost',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+    return FloatingActionButton.extended(
+      isExtended: ref.watch(isUserScrollingProvider),
+      tooltip: 'Create a new Nost',
+      label: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('NEW'),
+          SizedBox(width: 8.0),
+          Icon(FontAwesomeIcons.featherPointed),
+        ],
+      ),
+      onPressed: () async {
+        noteController.clear();
+        await showModalBottomSheet<void>(
+          context: context,
+          isScrollControlled: true,
+          builder: (BuildContext context) {
+            return Padding(
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    const Icon(FontAwesomeIcons.featherPointed),
+                    const SizedBox(height: 8.0),
+                    const Text(
+                      'Create a new Nost',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
                       ),
-                      const SizedBox(height: 16.0),
-                      TextFormField(
-                        controller: noteController,
-                        key: formKey,
-                        maxLines: 15,
-                        validator: (String? value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return 'Nost appears to be empty';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 16.0),
-                      if (widget.isNotePublishing)
-                        const Center(child: CircularProgressIndicator())
-                      else
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: const Text('Cancel'),
-                            ),
-                            const SizedBox(width: 16),
-                            ElevatedButton(
-                              onPressed: () {
-                                if (formKey.currentState!.validate()) {
-                                  widget
-                                      .publishNote(noteController.text.trim());
-                                } else {
-                                  formKey.currentState?.setState(() {});
-                                }
-                              },
-                              child: const Text('NOST!'),
-                            ),
-                          ],
-                        )
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: noteController,
+                      key: formKey,
+                      maxLines: 15,
+                      validator: (String? value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Nost appears to be empty';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    if (widget.isNotePublishing)
+                      const Center(child: CircularProgressIndicator())
+                    else
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel'),
+                          ),
+                          const SizedBox(width: 16),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                widget.publishNote(noteController.text.trim());
+                              } else {
+                                formKey.currentState?.setState(() {});
+                              }
+                            },
+                            child: const Text('NOST!'),
+                          ),
+                        ],
+                      )
+                  ],
                 ),
-              );
-            },
-          );
-        },
-      );
-    } else {
-      return FloatingActionButton(
-        tooltip: 'Create a new Nost',
-        child: const Icon(FontAwesomeIcons.featherPointed),
-        onPressed: () async {
-          noteController.clear();
-          await showModalBottomSheet<void>(
-            context: context,
-            isScrollControlled: true,
-            builder: (BuildContext context) {
-              return Padding(
-                padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      const Icon(FontAwesomeIcons.featherPointed),
-                      const SizedBox(height: 8.0),
-                      const Text(
-                        'Create a new Nost',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-                      TextFormField(
-                        controller: noteController,
-                        key: formKey,
-                        maxLines: 15,
-                        validator: (String? value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return 'Nost appears to be empty';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 16.0),
-                      if (widget.isNotePublishing)
-                        const Center(child: CircularProgressIndicator())
-                      else
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: const Text('Cancel'),
-                            ),
-                            const SizedBox(width: 16),
-                            ElevatedButton(
-                              onPressed: () {
-                                if (formKey.currentState!.validate()) {
-                                  widget
-                                      .publishNote(noteController.text.trim());
-                                } else {
-                                  formKey.currentState?.setState(() {});
-                                }
-                              },
-                              child: const Text('NOST!'),
-                            ),
-                          ],
-                        )
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      );
-    }
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 }
